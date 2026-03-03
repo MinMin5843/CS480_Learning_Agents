b5ded496d54abcfc8f0fc8a02ab8dccc5670b205
Lynda Salinas Ascanova <lsali002@odu.edu>
2026-03-03 18:38:32 -0500

    Fixed to include making the model folder

--- src/train.py
+++ src/train.py
@@ -1,12 +1,15 @@
-import torch
+import torch, os
 import torch.nn as nn
 import torch.optim as optim
-from model import DigitNet
+from src.model import DigitNet
 
 def train_model(hidden_size, train_loader, val_loader, epochs=30):
     model = DigitNet(hidden_size)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    os.makedirs("models", exist_ok=True)
+    os.makedirs("outputs", exist_ok=True)
 
     for epoch in range(epochs):
         model.train()
